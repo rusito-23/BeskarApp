6a62016bef24221abbe57e59e241702aa2afe700
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2021-01-28 19:57:57 -0300

    Initial Commit

---
+++ ExpenseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseApp
+//
+//  Created by Igor on 28/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
