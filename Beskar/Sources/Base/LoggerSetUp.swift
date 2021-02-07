4ed5012e4d544a55e4de0a17594f4a8c76725017
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2021-02-07 17:48:21 -0300

    Fix `pre-commit` and install pods

---
+++ Beskar/Sources/Base/LoggerSetUp.swift
@@ -0,0 +1,23 @@
+//
+//  LoggerSetUp.swift
+//  Beskar
+//
+//  Created by Igor on 07/02/2021.
+//
+
+import SwiftyBeaver
+
+// MARK: - Create Logger
+let log = SwiftyBeaver.self
+
+// MARK: - Setup
+extension SwiftyBeaver {
+    class func setup() {
+        #if DEBUG
+        let console = ConsoleDestination()
+        console.format = "$DHH:mm:ss$d $L: $M"
+        console.minLevel = .debug
+        log.addDestination(console)
+        #endif
+    }
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '14.0'
+workspace 'BeskarWorkspace'
+xcodeproj 'Beskar/Beskar.xcodeproj'
+
+target 'Beskar' do
+    use_frameworks!
+
+    pod 'RealmSwift'
+    pod 'TinyConstraints'
+    pod 'SwiftyBeaver'
+end
